4efb695a70e4aa0f998aa4c3e602137766909fdf
Your Name <you@example.com>
2018-11-24 21:41:37 -0500

    indented

--- gentest.py
+++ gentest.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python36 
+#!/usr/bin/env python3
 
 import json
 import sys
 import getopt
 import os
 
-from random import shuffle 
-
+from random import shuffle
 
 
 def testdata():
 
-    rc="""
+    rc = """
 
 {
      "form": {
@@ -66,64 +65,65 @@ def testdata():
     return rc
 
 
-
-
-
 class TestGen():
 
     data = None
 
-
     def __init__(self):
 
-        self.data = dict() 
+        self.data = dict()
 
         return
 
     def Load(self, filename):
 
-
         for i in filename:
             f = open(i, "r")
 
             if not f:
-               return
+                return
 
             rc = json.load(f)
             f.close()
 
             self.data.update(rc)
 
-        return self.data 
- 
+        return self.data
 
     def ProcessFormData(self):
 
-            f = ['first_name','last_name','date', 'course_name', 'course_number', 'test_title', 'instructor_name']
+        f = [
+            'first_name', 'last_name', 'date', 'course_name', 'course_number',
+            'test_title', 'instructor_name'
+        ]
 
-            if not self.data:
-
-                return
-
-            if 'form' in self.data:
-
-                for i in f:
-                    if i in self.data['form']:
-                        print("{}".format(self.data['form'][i]))
-
-            print("")
-            print("")
+        if not self.data:
 
             return
 
-    def ProcessTestQuestions(self, randomize=False, answerkey=False, r_just=3, l_just=20, **options):
+        if 'form' in self.data:
 
+            for i in f:
+                if i in self.data['form']:
+                    print("{}".format(self.data['form'][i]))
+
+        print("")
+        print("")
+
+        return
+
+    def ProcessTestQuestions(self,
+                             randomize=False,
+                             answerkey=False,
+                             r_just=3,
+                             l_just=20,
+                             **options):
 
         if not self.data:
             return
 
         if 'questions' not in self.data.keys():
-            print("Here") 
+            print("Here")
             return
 
         if randomize:
@@ -132,13 +132,17 @@ class TestGen():
         if 'number_of_questions' in options.keys():
             del self.data['questions'][options['number_of_questions']:]
 
-        for i,j in zip(self.data['questions'], range(1, len(self.data['questions']) + 1)):
+        for i, j in zip(self.data['questions'],
+                        range(1,
+                              len(self.data['questions']) + 1)):
 
             if i['type'] == "true_false":
 
                 if i['answer'] not in i['selections']:
-                     print(" Error: an \"answer\" exact match must be found within the \"selections\"")
-                     sys.exit(1)
+                    print(
+                        " Error: an \"answer\" exact match must be found within the \"selections\""
+                    )
+                    sys.exit(1)
                 print("{}. {}\n".format(j, i['question']))
                 for k, l in zip('abcdefghijklmonpqrstuvwxyz', i['selections']):
                     print("{}. {}".format(str.rjust(k, 3), l))
@@ -148,10 +152,10 @@ class TestGen():
             elif i['type'] == "multiple_choice":
                 print("{}. {}\n".format(j, i['question']))
                 for k, l in zip('abcdefghijklmonpqrstuvwxyz', i['selections']):
-                    print("{}. {}".format(str.rjust(k, 3),  l))
+                    print("{}. {}".format(str.rjust(k, 3), l))
                 if answerkey:
                     print("\nANSWER={}\n".format(i['answer']))
-                    
+
             elif i['type'] == 'fill_in_the_blank':
                 q = i['question'].format(**i)
                 print("{}. {}\n".format(j, q))
@@ -167,9 +171,9 @@ class TestGen():
 
                 if answerkey and 'answer' in i.items():
                     print("\nANSWER={}\n".format(i['answer']))
-                
+
             elif i['type'] == 'matching':
-                ## print(dir(i)) 
+                ## print(dir(i))
                 q = i['question'].format(**i)
                 print("{}. {}\n".format(j, q))
 
@@ -177,61 +181,66 @@ class TestGen():
                 y = list(i['key_values'].values())
 
                 shuffle(y)
-                shuffle(x) 
+                shuffle(x)
 
                 letters = (i for i in "abcdefghijklmnopqrstuvwxyz")
-                
-                for k,l in zip(x, y):
-                    print(" ___ {} {}. {}".format(str.ljust(k, 20), next(letters), str.rjust(l,0)))
+
+                for k, l in zip(x, y):
+                    print(" ___ {} {}. {}".format(
+                        str.ljust(k, 20), next(letters), str.rjust(l, 0)))
                 if answerkey:
                     print("")
                     for i, j, in i['key_values'].items():
-                            print("ANSWER {} = {}".format(i, j))
-                            
-                    
-                
-            print("")
+                        print("ANSWER {} = {}".format(i, j))
 
+            print("")
 
 
 def Usage(progname):
 
     progname = os.path.basename(progname)
-    
+
     print("")
-    print("\t{}: Formats and creates tests from JSON description file".format(progname))
+    print("\t{}: Formats and creates tests from JSON description file".format(
+        progname))
     print("")
     print("\tOptions:")
     print("")
     print("\t\t-h --help Print Help (this text)")
     print("\t\t-k --key  Produce Answer Key")
     print("\t\t-R --randomize Randomize the sequence of the questions")
-    print("\t\t-J --json-files <filename1,filename2,filename3> Read details from the named JSON file, or chain load")
-    print("\t\t-P --print-json Print Sample JSON test definitions (and then exit)")
-    print("\t\t-n --number <Number> only produce a test with number questions...")
+    print(
+        "\t\t-J --json-files <filename1,filename2,filename3> Read details from the named JSON file, or chain load"
+    )
+    print(
+        "\t\t-P --print-json Print Sample JSON test definitions (and then exit)"
+    )
+    print(
+        "\t\t-n --number <Number> only produce a test with number questions..."
+    )
     print("")
 
-
-    return         
+    return
 
 
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-            Usage(sys.argv[0])
-            sys.exit(1)
-            
+        Usage(sys.argv[0])
+        sys.exit(1)
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hkRJ:Pn:", ["help", "key", "randomize", "json-file", "print-json", "number"])
+        opts, args = getopt.getopt(
+            sys.argv[1:], "hkRJ:Pn:",
+            ["help", "key", "randomize", "json-file", "print-json", "number"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(str(err))  # will print something like "option -a not recognized"
         Usage(sys.argv[0])
         sys.exit(2)
 
-    options = dict() 
+    options = dict()
 
-    
     for o, a in opts:
         if o == "-v":
             verbose = True
@@ -245,28 +254,19 @@ if __name__ == "__main__":
         elif o in ("-J", "--json-file"):
             options["json_file"] = a.split(',')
         elif o in ("-P", "--print-json"):
-            print(testdata()) 
-            sys.exit(0)             
+            print(testdata())
+            sys.exit(0)
         elif o in ("-n", "--number"):
             options["number_of_questions"] = int(a)
         else:
             assert False, "unhandled option"
 
     if 'json_file' not in options.keys():
-            print("You need to pass in a JSON file for processing... ")
-            Usage(sys.argv[0])
-            sys.exit(1) 
-            
+        print("You need to pass in a JSON file for processing... ")
+        Usage(sys.argv[0])
+        sys.exit(1)
 
-            
     c = TestGen()
     c.Load(options['json_file'])
     c.ProcessFormData()
     c.ProcessTestQuestions(**options)
-
-    
-
-
-
-
-
